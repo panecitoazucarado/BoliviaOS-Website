af58838858d8cfb2ca689ff7538f3d270257e63c
panecitoazucarado <112211812+panecitoazucarado@users.noreply.github.com>
2026-08-30 23:09:45 -0400

    security: implement client-side security shield and HTTP security headers

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,6 +12,7 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Toaster } from "../components/ui/sonner";
+import { SecurityShield } from "../components/SecurityShield";
 
 function NotFoundComponent() {
   return (
@@ -151,6 +152,7 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <SecurityShield />
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
       <Toaster position="bottom-right" richColors />
